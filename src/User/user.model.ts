a55a030709f8b28956a289aa8db6718ec9c06966
Ivan Khomenko <simkav2411@gmail.com>
2022-03-19 20:38:56 +0200

    add user instance method checkPassword

--- src/User/user.model.ts
+++ src/User/user.model.ts
@@ -1,9 +1,9 @@
 import { isEmail } from 'class-validator'
 import { Schema, model } from 'mongoose'
-import { IUser } from './user.interface'
-import { hash } from 'bcrypt'
+import { IUser, IUserModel } from './user.interface'
+import { hash, compare } from 'bcrypt'
 
-const UserSchema = new Schema<IUser>({
+const UserSchema = new Schema<IUser, IUserModel>({
   email: {
     type: String,
     required: true,
@@ -20,6 +20,10 @@ UserSchema.pre('save', async function (next) {
   next()
 })
 
-const UserModel = model<IUser>('User', UserSchema)
+UserSchema.method('checkPassword', async function (password: string) {
+  return await compare(password, this.password)
+})
+
+const UserModel = model<IUser, IUserModel>('User', UserSchema)
 
 export { UserSchema, UserModel }
