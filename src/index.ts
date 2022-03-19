7d2cbcfcd312947308deb7c1650d2739a3ef2458
Ivan Khomenko <simkav2411@gmail.com>
2022-03-19 12:35:13 +0200

    add basic index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import dotenv from 'dotenv'
+import mongooseInit from './mongoose'
+import server from './server'
 dotenv.config()
-console.log('test')
+
+const PORT = process.env.PORT || 3000
+
+const init = async () => {
+  await mongooseInit()
+  server.listen(PORT, () => {
+    console.log(`server started on PORT: ${PORT}`)
+  })
+}
+
+init()
