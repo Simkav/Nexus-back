ef5e5464ad047f9aebd2bf64dc4f69ea22d99937
Ivan Khomenko <simkav2411@gmail.com>
2022-03-28 19:03:14 +0300

    add base custom error class

--- src/errorHandling.ts
+++ src/errorHandling.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
-import UserAlreadyExistsError from './User/errors/userAlreadyExists'
+import CustomError from './Errors/customError'
 const errorHandling = async (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof UserAlreadyExistsError) {
+  if (err instanceof CustomError) {
     res.status(400).send({ error: err.message })
   } else {
     console.log(err)
