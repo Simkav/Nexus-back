390dea2d5502887b98ea2ddb35e0aeee8ec49f10
Ivan Khomenko <simkav2411@gmail.com>
2022-03-30 17:41:20 +0300

    create mv for parsing auth header

---
+++ src/middlewares/parseJwt.mv.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response, Request } from 'express'
+import JwtService from '../Jwt/jwt.service'
+const jwtService = new JwtService()
+// TODO create errors
+const parseAuth = async (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization
+  if (!authHeader) {
+    throw new Error('No authorization header')
+  }
+  const [Bearer, token] = authHeader.split(' ')
+  if (!token) {
+    throw new Error('No token')
+  }
+  if (Bearer && Bearer !== 'Bearer') {
+    throw new Error('Schema must be a bearer')
+  }
+  try {
+    const payload = jwtService.verify(token)
+    req.jwtPayload = payload
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default parseAuth
