e6451b78f3f2f4d78787046853f0ad37a2978466
Ivan Khomenko <simkav2411@gmail.com>
2022-03-30 17:39:43 +0300

    working on mongoose model init

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,8 +1,11 @@
 import { connect } from 'mongoose'
+// TODO add parse models and import
+const arr = ['./Book/book.model', './User/user.model']
 
 const init = async () => {
   try {
     await connect(process.env.MONGO_CONNECT_URL || '')
+    await Promise.all(arr.map(module => import(module)))
     console.log('Mongoose connected')
   } catch (error) {
     console.error(error)
