5c8f1fc3d69acab67649fefc5fed1fef7cf3e515
Ivan Khomenko <simkav2411@gmail.com>
2022-04-05 12:31:41 +0300

    fix trycatch in parseJwt mv

--- src/middlewares/parseJwt.mv.ts
+++ src/middlewares/parseJwt.mv.ts
@@ -3,18 +3,18 @@ import JwtService from '../Jwt/jwt.service'
 const jwtService = new JwtService()
 // TODO create errors
 const parseJwt = async (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization
-  if (!authHeader) {
-    throw new Error('No authorization header')
-  }
-  const [Bearer, token] = authHeader.split(' ')
-  if (!token) {
-    throw new Error('No token')
-  }
-  if (Bearer && Bearer !== 'Bearer') {
-    throw new Error('Schema must be a bearer')
-  }
   try {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+      throw new Error('No authorization header')
+    }
+    const [Bearer, token] = authHeader.split(' ')
+    if (!token) {
+      throw new Error('No token')
+    }
+    if (Bearer && Bearer !== 'Bearer') {
+      throw new Error('Schema must be a bearer')
+    }
     const payload = jwtService.verify(token)
     req.jwtPayload = payload
     next()
