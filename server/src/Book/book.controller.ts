859767624f5b162e4fc7a13931d86a138a4c66f6
Ivan Khomenko <simkav2411@gmail.com>
2022-04-08 15:11:58 +0300

    add route for updateBook

--- server/src/Book/book.controller.ts
+++ server/src/Book/book.controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Response, Request, Router } from 'express'
 import parseJwt from '../middlewares/parseJwt.mv'
 import validationMw from '../middlewares/validaton.mv'
 import UserService from '../User/user.service'
-import { CreateBookDto, AddCommentDto } from './book.interface'
+import { CreateBookDto, AddCommentDto, UpdateBookDto } from './book.interface'
 import BookService from './book.service'
 
 class BookController {
@@ -29,6 +29,20 @@ class BookController {
       res.send({ book })
     } catch (error) {
       next(error)
+    }
+  }
+  updateBook = async (req: Request, res: Response, next: NextFunction) => {
+    const {
+      params: { bookId },
+      body
+    } = req
+    const user = await this.userService.findUserById(req.jwtPayload.userId)
+    if (user.books.some(book => book._id.toString() === bookId)) {
+      const book = await this.bookService.findBookById(bookId)
+      const result = await this.bookService.updateBook(book, body)
+      res.send({ success: result })
+    } else {
+      next(new Error('You are not allowed to remove comment from this book'))
     }
   }
   deleteBook = async (req: Request, res: Response, next: NextFunction) => {
@@ -106,6 +120,7 @@ BookRouter.route('/:bookId')
   .all(parseJwt)
   .get(bookController.getBookById)
   .delete(bookController.deleteBook)
+  .patch(validationMw(UpdateBookDto), bookController.updateBook)
 BookRouter.post(
   '/:bookId/comment',
   validationMw(AddCommentDto),
