3a93ba3766ab0898bd39ad350e1d9585392189bf
Ivan Khomenko <simkav2411@gmail.com>
2022-04-08 12:18:42 +0300

    Remove properties from body that are not in dto

--- server/src/middlewares/validaton.mv.ts
+++ server/src/middlewares/validaton.mv.ts
@@ -1,10 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
 import { validate } from 'class-validator'
-import { plainToInstance } from 'class-transformer'
+import { instanceToPlain, plainToInstance } from 'class-transformer'
 
 const validationMw = (dtoClass: any) =>
   function (req: Request, res: Response, next: NextFunction) {
-    const output: any = plainToInstance(dtoClass, req.body)
+    const output: any = plainToInstance(dtoClass, req.body, {
+      strategy: 'excludeAll'
+    })
     validate(output, { skipMissingProperties: true }).then(errors => {
       if (errors.length) {
         console.log(errors)
@@ -15,6 +17,7 @@ const validationMw = (dtoClass: any) =>
         res.status(400).send(errorTexts)
         return
       } else {
+        req.body = instanceToPlain(output, { strategy: 'excludeAll' })
         next()
       }
     })
